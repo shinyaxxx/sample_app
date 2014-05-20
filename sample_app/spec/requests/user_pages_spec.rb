1a801411d29c21b74865cff6ce485d1399df912f
Shinnya <y10e105743@gmail.com>
2014-05-20 20:17:59 +0900

    Writed test code for registration page.

--- sample_app/spec/requests/user_pages_spec.rb
+++ sample_app/spec/requests/user_pages_spec.rb
@@ -19,4 +19,29 @@ describe "UserPages" do
     it { should have_title('Sign up') }
   end
 
+  describe "signup" do
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button subject }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        example {click_button subject}.to change(User, :count).by(1)
+      end
+    end
+  end
+
 end
