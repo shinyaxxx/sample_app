1f70917385e8d8cef68b81c5b6175223ea65e197
Shinnya <y10e105743@gmail.com>
2014-05-20 19:46:04 +0900

    Finished section 7.1

--- sample_app/config/environments/test.rb
+++ sample_app/config/environments/test.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -36,4 +37,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # bcrypt'のコスト関数を下げることでテストの速度を向上させる。
+  ActiveModel::SecurePassword.min_cost = true
 end
