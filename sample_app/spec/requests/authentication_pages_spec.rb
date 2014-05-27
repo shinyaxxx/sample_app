9c9d52931b22e801d49ffd284ccf9795fd43bdad
Shinnya <y10e105743@gmail.com>
2014-05-27 20:19:27 +0900

    Added index page

--- sample_app/spec/requests/authentication_pages_spec.rb
+++ sample_app/spec/requests/authentication_pages_spec.rb
@@ -31,9 +31,10 @@ describe "Authentication" do
       before { valid_signin(user) }
 
       it { should have_title(user.name) }
-      it { should have_link('Profile', href: user_path(user)) }
-      it { should have_link('Settings', href: edit_user_path(user)) }
-      it { should have_link('Sign out', href: signout_path) }
+      it { should have_link('Users',       href: users_path) }
+      it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
+      it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
 
       describe "followed by signout" do
@@ -74,6 +75,11 @@ describe "Authentication" do
         describe "submitting to the update action" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "visiting the user index" do
+          before  { visit users_path }
+          it { should have_title('Sign in') }
         end
       end
     end
