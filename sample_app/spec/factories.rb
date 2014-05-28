f9c1692378b15483cbc6b324fd7c90fffb367600
Shinnya <y10e105743@gmail.com>
2014-05-28 09:36:30 +0900

    Finished Sec 9.3

--- sample_app/spec/factories.rb
+++ sample_app/spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-     name "Michael Hartl"
-     email "michael@example.com"
-     password "foobar"
-     password_confirmation "foobar"
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email) { |n| "Person_#{n}@example.com" }
+    password "foobar"
+    password_confirmation "foobar"
   end
 end
